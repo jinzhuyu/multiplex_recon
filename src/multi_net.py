bcb1cdd6264a2672dbb2e2dc7008ed5a3bc34edd
Jin-Zhu Yu <yujinzhu88@gmail.com>
2022-06-09 00:19:21 -0400

    update reconstruct

--- src/multi_net.py
+++ src/multi_net.py
@@ -92,112 +92,131 @@ multi_net.gen_net()
 
 class Reconstruct:
     
-    
-    def recon_multiplex(M1, M2, PONset1, PONset2, N, itermax=1000, eps=1e-6):   
+    def recon_multiplex(M_list, PONset_list, n_node, itermax=1000, eps=1e-6):
+        '''     
+        Parameters
+        ----------
+        M1, M2: the adjacency matrices of layer 1, 2 in a multiplex
+        PONset1, PONset2 : node set containing the index of nodes in the subgraphs
+        n_node : number of nodes in each layer. nodes are shared over layers.
+
+        Returns
+        -------
+        Q1,Q2: the reconstructed adjacency matrices of layer 1, 2 in the multiplex network
+
+        '''
         
-        #generate the ground truth of a multiplex network
-        A1 = np.zeros([N,N]) 
-        A2 = np.zeros([N,N])  
-    
-        # JZ: layers should have same nodeset. Use the Union of sets of nodes in each layer.
-        # Why do we have the aggregated topology? And why do we need the aggregated topology?
+        # TODO: what if there are many layers instead of 2
+            # the input M and PONset should be list
+            # n_node can be inferred from input
         
-        for k in range(N):
-            i = M1[k, 1] 
-            j = M1[k, 2] 
-            A1[i, j] = 1 
-            A1[j, i] = 1 
+        # generate the ground truth of a multiplex network
+        n_node = self.n_node
+        n_layer = len(self.M_list)
         
-        for k in range(N):
-            i = M1[k, 1] 
-            j = M1[k, 2]
-            A2[i, j]=1 
-            A2[j, i]=1 
+        self.A2 = np.zeros([n_node,n_node])  
     
-        #generate the aggregate network
-        J_N = np.ones([N,N])
-        A0 = J_N - (J_N - A1)*(J_N - A2) 
+        # layers should have same nodeset. Use the Union of sets of nodes in each layer.
+        def get_layer_adj(self, M):
+            n_node = self.n_node
+            A = np.zeros([n_node, n_node]) 
+            for k in range(n_node):
+                i = M[k, 1] 
+                j = M[k, 2] 
+                A[i, j] = 1 
+                A[j, i] = 1
+            return A
+        
+        layer_adj_list = []
+        for ix in range(n_layer):
+            layer_adj_list.append(get_layer_adj(M_list[idx]))
+    
+        # generate the aggregate network
+        J_N = np.ones([self.n_node, self.n_node])
+        self.A0 = J_N - (J_N - self.A1)*(J_N - self.A2) 
         
         #initial guess of the network model parameters
-        deg_seq_1 = np.random.randint(low=1,high=N+1, size=N)
-        deg_seq_2 = np.random.randint(low=1,high=N+1, size=N) 
-        deg_seq_last_1 = np.zeros(N) 
-        deg_seq_last_2 = np.zeros(N)  
+        deg_seq_list = []
+        deg_seq_last_list = []
+        for idx in range(n_layer):
+            self.deg_seq_list.append( np.random.randint(low=1, high=self.n_node+1, size=self.n_node) )
+            self.deg_seq_last_list.append( np.zeros(self.n_node) ) 
     
-        for iter in range(itermax):
+        for iter in range(self.itermax):
     
-            Q1 = np.zeros(N) 
-            Q2 = np.zeros(N) 
-            DegreeSum1 = np.sum(deg_seq_1)-1 
-            DegreeSum2 = np.sum(deg_seq_2)-1 
+            Q1 = np.zeros(n_node) 
+            Q2 = np.zeros(n_node) 
+            DegreeSum1 = np.sum(self.deg_seq_1)-1 
+            DegreeSum2 = np.sum(self.deg_seq_2)-1 
     
             #calculate link reliabilities by configuration model
-            for i in range(N):
-                for j in range(N):
-                    dem = 1-(1-deg_seq_1(i)*deg_seq_1(j)/DegreeSum1)*(1-deg_seq_2(i)*deg_seq_2(j)/DegreeSum2) 
-                    if dem==0:
-                        Q1[i, j]=0 
-                        Q2[i, j]=0 
-                    if dem != 0:
-                        Q1[i, j]=A0[i, j]*deg_seq_1(i)*deg_seq_1(j)/DegreeSum1/dem 
-                        Q2[i, j]=A0[i, j]*deg_seq_2(i)*deg_seq_2(j)/DegreeSum2/dem 
+            def cal_link_prob(self):
+                for i in range(n_node):
+                    for j in range(n_node):
+                        dem = 1-(1-self.deg_seq_1(i)*self.deg_seq_1(j)/DegreeSum1)*(1-self.deg_seq_2(i)*self.deg_seq_2(j)/DegreeSum2) 
+                        if dem==0:
+                            Q1[i, j]=0 
+                            Q2[i, j]=0 
+                        if dem != 0:
+                            Q1[i, j]=self.A0[i, j]*self.deg_seq_1(i)*self.deg_seq_1(j)/DegreeSum1/dem 
+                            Q2[i, j]=self.A0[i, j]*self.deg_seq_2(i)*self.deg_seq_2(j)/DegreeSum2/dem 
     
             #avoid probability overflow in configuration model
             def remove_error_prob(self, Q):
                 Q[Q<0]=0 
-                Q[Q1>1]=1 
+                Q[Q>1]=1 
                 
-            Q1 = self.remove_error_prob(Q1)
-            Q2 = self.remove_error_prob(Q2)
+            self.Q1 = self.remove_error_prob(self.Q1)
+            self.Q2 = self.remove_error_prob(self.Q2)
     
     
             #patial observation in layer 1
-            for s in range(len(PONset1)):
-                for t in range(len(PONset1)):
+            for s in range(len(self.PONset1)):
+                for t in range(len(self.PONset1)):
     
-                    i = PONset1(s) 
-                    j = PONset1(t) 
-                    Q1[i, j] = A1[i, j] 
+                    i = self.PONset1(s) 
+                    j = self.PONset1(t) 
+                    self.Q1[i, j] = self.A1[i, j] 
     
                     #OR-aggregate mechanism
-                    if A0[i, j] == 1:
+                    if self.A0[i, j] == 1:
                         if Q1[i, j] == 1:
                             if Q2[i, j]!=0 and Q2[i, j]!=1:
-                                Q2[i, j]=deg_seq_2(i)*deg_seq_2(j)/DegreeSum2
+                                Q2[i, j]=self.deg_seq_2(i)*self.deg_seq_2(j)/DegreeSum2
                         if Q1[i, j]==0:
                             Q2[i, j] = 1 
    
             #patial observation in layer 2
-            for s in range(len(PONset1)):
-                for t in range(len(PONset1)):
+            for s in range(len(self.PONset1)):
+                for t in range(len(self.PONset1)):
     
-                    i = PONset2(s) 
-                    j = PONset2(t) 
-                    Q2[i, j] = A2[i, j] 
+                    i = self.PONset2(s) 
+                    j = self.PONset2(t) 
+                    self.Q2[i, j] = self.A2[i, j] 
     
                     #OR-aggregate mechanism
-                    if A0[i, j]==1:
+                    if self.A0[i, j]==1:
                         if Q2[i, j]==1:
                             if Q1[i, j]!=0 and Q1[i, j]!=1:
-                                Q1[i, j]=deg_seq_1(i)*deg_seq_1(j)/DegreeSum1 
+                                Q1[i, j]=self.deg_seq_1(i)*self.deg_seq_1(j)/DegreeSum1 
                         if Q2[i, j]==0:
                             Q1[i, j] = 1 
    
             #avoid probability overflow in configuration model
-            Q1 = self.remove_error_prob(Q1)
-            Q2 = self.remove_error_prob(Q2)    
+            self.Q1 = self.remove_error_prob(self.Q1)
+            self.Q2 = self.remove_error_prob(self.Q2)   
     
             #update network model parameters
-            deg_seq_1 = np.sum(Q1) 
-            deg_seq_2 = np.sum(Q2) 
+            self.deg_seq_1 = np.sum(self.Q1) 
+            self.deg_seq_2 = np.sum(self.Q2) 
     
     
-            #convergence judgement
-            if np.sum(np.abs(deg_seq_last_1-deg_seq_1))<eps and\
-               np.sum(np.abs(deg_seq_last_2-deg_seq_2))<eps: 
+            #convergence check
+            if np.sum(np.abs(self.deg_seq_last_1-self.deg_seq_1))<self.eps and\
+               np.sum(np.abs(self.deg_seq_last_2-self.deg_seq_2))<self.eps: 
                 break
-            deg_seq_last_1 = deg_seq_1 
-            deg_seq_last_2 = deg_seq_2 
+            self.deg_seq_last_1 = self.deg_seq_1 
+            self.deg_seq_last_2 = self.deg_seq_2 
     
         
     
